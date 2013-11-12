8db72b297e8077912f1b7fbc8909e806d0d99d4c
Alex Ivanov <alex@ivanov.es>
2013-11-11 20:50:25 -0600

    text area for future stack trace

--- agent/src/main/java/com/swingInspector/ui/SwingDevelopmentConsoleUI.java
+++ agent/src/main/java/com/swingInspector/ui/SwingDevelopmentConsoleUI.java
@@ -18,11 +18,10 @@ import static com.swingInspector.runtime.ComponentHighlightConfiguration.BorderT
 public class SwingDevelopmentConsoleUI extends JFrame {
 	private final AtomicBoolean bordersHighlight = new AtomicBoolean(false);
 	private final JButton enableDisableBorderButton;
+	private final JTextArea stackTracePane;
 
 	public SwingDevelopmentConsoleUI() throws HeadlessException {
 		super("Swing development console");
-		JPanel basePanel = new JPanel(new GridBagLayout());
-
 		enableDisableBorderButton = new JButton("Enable locator");
 		enableDisableBorderButton.addActionListener(new ActionListener() {
 			@Override
@@ -34,8 +33,25 @@ public class SwingDevelopmentConsoleUI extends JFrame {
 				}
 			}
 		});
-		basePanel.add(enableDisableBorderButton);
+
+		stackTracePane = new JTextArea("");
+		stackTracePane.setEditable(false);
+		stackTracePane.setRows(200);
+		stackTracePane.setColumns(200);
+
+		JPanel basePanel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		basePanel.add(enableDisableBorderButton, c);
+		c.fill = GridBagConstraints.BOTH;
+		c.gridy = 1;
+		c.gridx = 0;
+		c.weightx = 1;
+		c.weighty = 1;
+		basePanel.add(stackTracePane, c);
+
 		this.getContentPane().add(basePanel);
+		this.setMaximumSize(new Dimension(400, 400));
 		pack();
 	}
 
