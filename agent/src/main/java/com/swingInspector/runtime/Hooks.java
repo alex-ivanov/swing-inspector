038ce5289f447268e3b737ac92377f272adac67e
Alex Ivanov <alex@ivanov.es>
2013-11-12 18:21:59 -0600

    javadoc

--- agent/src/main/java/com/swingInspector/runtime/Hooks.java
+++ agent/src/main/java/com/swingInspector/runtime/Hooks.java
@@ -6,7 +6,11 @@ import javax.swing.*;
  * author: alex
  * date  : 11/12/13
  */
+@SuppressWarnings("UnusedDeclaration")
 public class Hooks {
+	/**
+	 * This method will be called on each JComponent constructor call.
+	 */
 	public static void onComponentCreate(JComponent c) {
 		SwingInspectorConsole.components.register(c);
 	}
