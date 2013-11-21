bb1f42048cba27c98cf9d4eea4dc2e44eef0c692
Alex Ivanov <alex@ivanov.es>
2013-11-21 12:34:20 -0600

    more complex example

--- agent/src/test/java/TestSwing.java
+++ agent/src/test/java/TestSwing.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * author: alex
@@ -7,11 +8,31 @@ import javax.swing.*;
 public class TestSwing {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("hello");
-		JPanel panel = new JPanel();
-		panel.add(new JButton("Hello A"));
-		panel.add(new JButton("Hello B"));
+		JPanel mainPanel = new JPanel(new GridBagLayout());
 
-		frame.getContentPane().add(panel);
+		JPanel simpleButtonPanel = new JPanel();
+		simpleButtonPanel.add(new JButton("Hello A"));
+		simpleButtonPanel.add(new JButton("Hello B"));
+
+		JPanel simpleTextComponentsPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		simpleTextComponentsPanel.add(new JTextField("Simple text field"), c);
+
+		c.gridy = 1;
+		c.fill = GridBagConstraints.BOTH;
+		simpleTextComponentsPanel.add(new JTextArea("Long text area", 20, 30), c);
+
+		GridBagConstraints c2 = new GridBagConstraints();
+		c2.gridx = 0;
+		c2.gridy = 0;
+		mainPanel.add(simpleButtonPanel, c2);
+		c2.gridy = 1;
+		mainPanel.add(simpleTextComponentsPanel, c2);
+
+		frame.getContentPane().add(mainPanel);
 
 		frame.pack();
 		frame.setVisible(true);
